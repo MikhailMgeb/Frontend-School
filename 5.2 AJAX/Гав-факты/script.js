c55a2d0d93ec1da163a0cb0f240641fe59704b9a
Michael Mgebrishvili <mmd920802@gmail.com>
2023-08-30 01:03:20 +0300

    start do homework gav

--- 5.2 AJAX/Гав-факты/script.js
+++ 5.2 AJAX/Гав-факты/script.js
@@ -1,1 +1,14 @@
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
 
+button.addEventListener('click', function (event) {
+    button.disabled = true;
+    const number = input.value
+
+    fetch('https://dog-api.kinduff.com/api/facts', {
+        method: 'GET',
+        number: input.value,
+    })
+        .then((response) => response.json)
+        .then((data) => console.log(data))
+})
