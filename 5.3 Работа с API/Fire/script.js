8f09ae194ab7b348afd06cd816871282234b6cf0
Michael Mgebrishvili <mmd920802@gmail.com>
2023-09-03 20:59:55 +0300

    finish first exsersize

--- 5.3 Работа с API/Fire/script.js
+++ 5.3 Работа с API/Fire/script.js
@@ -2,7 +2,19 @@ const foxGallery = document.querySelector('.fox-gallery');
 const foxAddButton = document.querySelector('.fox-gallery__button');
 
 foxAddButton.addEventListener('click', (event) => {
+    foxAddButton.disabled = true;
+    foxAddButton.classList.add('load');
+
     fetch('https://randomfox.ca/floof/')
         .then(resolve => resolve.json())
-        .then(result => console.log(result))
+        .then(result => {
+            foxAddButton.disabled = false;
+
+            const image = document.createElement('img');
+            image.classList.add('fox-gallery__photo')
+            image.src = result.image;
+
+            foxGallery.appendChild(image);
+            foxAddButton.classList.remove('load');
+        })
 })
