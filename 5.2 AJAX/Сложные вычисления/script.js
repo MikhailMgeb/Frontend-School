9dc3bbac5dd374ac82220a13e386ff510e98e82d
Michael Mgebrishvili <mmd920802@gmail.com>
2023-09-07 00:03:58 +0300

    finish math-summ

--- 5.2 AJAX/Сложные вычисления/script.js
+++ 5.2 AJAX/Сложные вычисления/script.js
@@ -1,16 +1,30 @@
 const blockMath = document.querySelector('.block-math');
+const button = document.querySelector('.block-math__button');
 const inputFirst = document.querySelector('.input-first');
 const inputSecond = document.querySelector('.input-second');
-const resultNumber = document.createElement('p');
-blockMath.appendChild(resultNumber);
+const resultNumberBlock = document.querySelector('.result-number');
+const spanLoader = document.querySelector('.block-math__span');
 
-blockMath.addEventListener('input', (event) => {
-    resultNumber.classList.remove('error');
+button.addEventListener('click', () => {
+    inputFirst.disabled = true;
+    inputSecond.disabled = true;
+    button.disabled = true;
+
+    spanLoader.classList.add('loader');
+    const resultNumber = document.createElement('p');
+    resultNumberBlock.innerHTML = '';
 
     fetch(`http://localhost:3000/?first=${inputFirst.value}&second=${inputSecond.value}`)
         .then((response) => response.json())
         .then((result) => {
-            resultNumber.textContent = result.result;
+            spanLoader.classList.remove('loader');
+            inputFirst.disabled = false;
+            inputSecond.disabled = false;
+            button.disabled = false;
+
+            resultNumber.classList.add('result-number__text');
+            resultNumber.textContent = 'Итого: ' + result.result;
+            resultNumberBlock.appendChild(resultNumber);
         })
         .catch(() => {
             resultNumber.textContent = 'Введите число';
