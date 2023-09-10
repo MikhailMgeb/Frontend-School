f949a861a46e1d3b394a4c1b931a6b9437046c97
Michael Mgebrishvili <mmd920802@gmail.com>
2023-09-10 22:17:23 +0300

    finish fish

--- 3.6 События. Обработка форм/Курица или рыба/index.js
+++ 3.6 События. Обработка форм/Курица или рыба/index.js
@@ -1,70 +1,46 @@
-const form = document.querySelector('.registration-form')
-const formField = form.querySelector('.form-field')
+const form = document.querySelector('.registration-form');
+const formField = form.querySelector('.form-field');
 const registrationButton = form.querySelector('.registration-form__button');
 const checkboxFood = form.querySelector('.checkbox-food');
 const isSelectFood = form.querySelector('.registration-form_select-food');
+const message = document.querySelector('.message');
+
+const errorBlock = form.querySelector('.registration-form__error')
+errorBlock.classList.add('hidden');
+message.classList.add('hidden');
 
 const registrationData = {};
-
-function isCorrectChoice(formField) {
-    const letters = 'ABCDEF';
-    const spanСhoiceSeat = document.createElement('span')
-
-    if (letters.indexOf(registrationData.seat[0])) {
-        spanСhoiceSeat.classList.add('choice-seat-error');
-        spanСhoiceSeat.textContent = 'Первая буква должна содержать "ABCDEF"';
-
-        registrationButton.disabled = true;
-    } else {
-        registrationButton.disabled == false;
-    }
-
-    if (checkboxFood.checked === true) {
-        isSelectFood.disabled = false;
-    } else {
-        isSelectFood.disabled = true;
-    }
-}
-
-function сhoiceFood(formField) {
-    if (formField.checked === true) {
-        console.log('check-food');
-    }
-}
-
-const validators = {
-    seat: isCorrectChoice,
-    food: сhoiceFood,
-}
-
-function validateFormField(formField) {
-    const validator = validators[formField.name];
-
-    if (!validator) {
-        return true;
-    }
-
-    const error = validator(formField);
-    
-    const fieldContainer = formField.closest('.form-field');
-    const errorElem = fieldContainer.querySelector('.registration-form__error');
-    errorElem.classList.add('hidden');
-
-    if (error === nell) {
-        return true;
-    }
-
-    errorElem.classList.remove('hidden');
-    errorElem.textContent = error;
-
-}
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-})
+registrationButton.disabled = true;
 
 form.addEventListener('input', (event) => {
     const target = event.target;
     const key = target.name;
     registrationData[key] = target.type === 'checkbox' ? target.checked : target.value;
+
+    if (registrationData['food-agree'] === true) {
+        isSelectFood.disabled = false;
+    } else {
+        isSelectFood.disabled = true;
+    }
+
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+    if (!letters.includes(registrationData.seat[0])) {
+        errorBlock.innerHTML = '';
+        const errorSeat = document.createElement('span');
+        errorSeat.classList.add('choice-seat-error');
+        errorSeat.textContent = 'Первая буква должна содержать "ABCDEF"';
+        errorBlock.appendChild(errorSeat);
+        errorBlock.classList.remove('hidden');
+    } else {
+        registrationButton.disabled = false;
+        errorBlock.innerHTML = '';
+        errorBlock.classList.add('hidden');
+    }
+})
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    message.classList.remove('hidden');
+    message.textContent = `Поздравляем с успешной регистрацией на рейс ваше место ${registrationData.seat}`;
 })
