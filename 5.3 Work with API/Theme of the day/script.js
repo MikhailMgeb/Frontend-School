c8ea61097125da9c0e598d4d58b0e89db493d865
Michael Mgebrishvili <mmd920802@gmail.com>
2023-09-24 00:53:13 +0300

    fix theme function

--- 5.3 Work with API/Theme of the day/script.js
+++ 5.3 Work with API/Theme of the day/script.js
@@ -1,30 +1,31 @@
-const page = document.querySelector('.page')
+const page = document.querySelector(".page");
 
-fetch('https://api.wordnik.com/v4/words.json/randomWord?api_key=luxfw7rqui299pwpwdflamw9baggz1zl55jm0dpxqjz1h3ym6')
-    .then(response => response.json())
-    .then(word => {
-        console.log(word.word)
-        const image = document.createElement('img');
-        image.classList.add('theme');
-        fetch(`https://api.unsplash.com/search/photos?query=${word.word}&client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`)
-            .then(response => response.json())
-            .then(url => {
+fetch(
+  "https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=luxfw7rqui299pwpwdflamw9baggz1zl55jm0dpxqjz1h3ym6"
+)
+  .then((response) => response.json())
+  .then((word) => {
+    const image = document.createElement("img");
+    image.classList.add("theme");
 
-                image.src = url.results[0].urls.full;
-                page.appendChild(image);
-
-            })
-            .catch(() => {
-                fetch(`https://api.unsplash.com/photos/random?client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`)
-                    .then(response => response.json())
-                    .then(url => {
-
-                        image.src = url.results[0].urls.full;
-                        page.appendChild(image);
-
-                    })
-                    .catch(console.log('нет фотки!!!'))
-
-            })
-    })
-
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${word.word}&client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`
+    )
+      .then((response) => response.json())
+      .then((url) => {
+        image.src = url.results[0].urls.full;
+        page.appendChild(image);
+      })
+      .catch(() => {
+        
+        fetch(
+          `https://api.unsplash.com/photos/random?client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`
+        )
+          .then((response) => response.json())
+          .then((url) => {
+            image.src = url.urls.full;
+            page.appendChild(image);
+          })
+          .catch(console.log("нет фотки!!!"));
+      });
+  });
