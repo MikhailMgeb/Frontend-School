7188466af16b6bea926ce703e862e3030121a28a
Michael Mgebrishvili <mmd920802@gmail.com>
2023-10-14 18:55:12 +0300

    refactor function getUser

--- 5.1 Async logics/Promisrepresentation/script.js
+++ 5.1 Async logics/Promisrepresentation/script.js
@@ -5,35 +5,36 @@ const nameBlock = document.querySelector('.name-block');
 const greetings = document.createElement('p');
 
 input.classList.add('name-input');
-button.classList.add('name-buttom');
+button.classList.add('name-button');
 
 function getUserName() {
-    if (window.localStorage.getItem('name')) {
-        return window.localStorage.getItem('name');
-    } else {
-        return new Promise((resolve, reject) => {
-            button.textContent = 'Подтверждаю!';
-            input.placeholder = 'Ваше имя'
+  if (window.localStorage.getItem('name')) {
+    return new Promise((resolve, reject) => {
+      resolve(window.localStorage.getItem('name'));
+    });
+  } else {
+    return new Promise((resolve, reject) => {
+      button.textContent = 'Подтверждаю!';
+      input.placeholder = 'Ваше имя';
 
-            nameBlock.appendChild(input);
-            nameBlock.appendChild(button);
-            resolve(window.localStorage.setItem('name', input.value));
-        })
-    }
+      nameBlock.appendChild(input);
+      nameBlock.appendChild(button);
+
+      button.addEventListener('click', () => {
+        if (input.value === '') {
+          return;
+        }
+        window.localStorage.setItem('name', input.value);
+        resolve(input.value);
+      });
+    });
+  }
 }
 
-button.addEventListener('click', () => {
-    if (input.value === '') {
-        return;
-    }
-
-    getUserName().then(() => {
-        greetings.textContent = 'Добро пожаловать, ' + localStorage.getItem('name');
-        greetings.classList.add('popap');
-        nameBlock.appendChild(greetings);
-        input.remove();
-        button.remove();
-    })
-})
-
-getUserName();
+getUserName().then((name) => {
+  greetings.textContent = 'Добро пожаловать, ' + name;
+  greetings.classList.add('popap');
+  nameBlock.appendChild(greetings);
+  input.remove();
+  button.remove();
+});
