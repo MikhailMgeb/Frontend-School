2e73e1babf28b2861ed9c2f74ef64153a160e7b7
Michael Mgebrishvili <mmd920802@gmail.com>
2023-10-30 23:58:11 +0300

    create fetchs requars

--- 5.3 Work with API/Digitization/script.js
+++ 5.3 Work with API/Digitization/script.js
@@ -1,11 +1,83 @@
 import { openKey, secretKey, headField } from './keysApi.js';
 
-fetch('https://api.ilovepdf.com/v1/auth', {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify(headField),
+const conversionInput = document.querySelector('.conversion__input')
+const conversionButton = document.querySelector('.conversion__button')
+
+
+conversionButton.addEventListener('click', () => {
+    fetch('https://api.ilovepdf.com/v1/auth', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(headField),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            // console.log('data: ', data.token);
+            const bearerToken = data.token;
+
+            return fetch('https://api.ilovepdf.com/v1/start/htmlpdf', {
+                headers: {
+                    'Authorization': `Bearer ${bearerToken}`,
+                }
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    const requestServer = data.server;
+                    const requestTask = data.task;
+                    // console.log(requestServer, requestTask)
+
+                    fetch(`https://${requestServer}/v1/upload`, {
+                        method: "POST",
+                        headers: {
+                            'Authorization': `Bearer ${bearerToken}`,
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            "task": requestTask,
+                            "cloud_file": conversionInput.value,
+                        })
+                    })
+                        .then((response) => response.json())
+                        .then((data) => {
+                            const serverFilename = data['server_filename'];
+
+                            fetch(`https://${requestServer}/v1/process`, {
+                                method: "POST",
+                                headers: {
+                                    'Authorization': `Bearer ${bearerToken}`,
+                                    "Content-Type": "application/json",
+                                },
+                                body: JSON.stringify({
+                                    'task': requestTask,
+                                    'tool': 'htmlpdf',
+                                    'files': [
+                                        {
+                                            'server_filename': serverFilename,
+                                            'filename': 'server.pdf'
+                                        }
+                                    ]
+                                })
+                            })
+                                .then((response) => response.json())
+                                .then((data) => {
+                                    console.log(data);
+
+                                    fetch(`https://${requestServer}/v1/download/${requestTask}`, {
+                                        headers: {
+                                            'Authorization': `Bearer ${bearerToken}`,
+
+                                        },
+                                    })
+                                        .then((resp) => {
+                                            return resp.blob();
+                                        })
+                                        .then((blob) => {
+                                            download(blob);
+                                        })
+                                })
+                        })
+                })
+        })
 })
-    .then((response) => response.json())
-    .then((data) => console.log('data: ', data))
