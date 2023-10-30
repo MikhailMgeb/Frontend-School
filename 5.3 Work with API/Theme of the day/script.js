9d8573cee49753176bb39264e68bd184412e118d
Michael Mgebrishvili <mmd920802@gmail.com>
2023-10-30 23:15:02 +0300

    fix

--- 5.3 Work with API/Theme of the day/script.js
+++ 5.3 Work with API/Theme of the day/script.js
@@ -1,14 +1,14 @@
 import { API_KEY } from './apiKey.js';
 
 const page = document.querySelector('.page');
+const image = document.createElement('img');
 
 fetch(
-  `https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=${API_KEY }`)
+  `https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=${API_KEY}`)
   .then((response) => response.json())
   .then((word) => {
-    const image = document.createElement('img');
     image.classList.add('theme');
-    fetch(
+    return fetch(
       `https://api.unsplash.com/search/photos?query=${word.word}&client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`
     )
       .then((response) => response.json())
@@ -16,19 +16,19 @@ fetch(
         image.src = url.results[0].urls.full;
         page.appendChild(image);
       })
-      .catch(() => {
-        fetch(
-          `https://api.unsplash.com/photos/random?client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`
-        )
-          .then((response) => response.json())
-          .then((url) => {
-            image.src = url.urls.full;
-            page.appendChild(image);
-          })
-          .catch(console.log('нет фотки!!!'));
-      });
+      .catch(() => {'error!'});
   })
   .finally(() => {
     const loader = document.querySelector('.lds-ring');
     loader.classList.add('hidden');
   });
+
+fetch(
+  `https://api.unsplash.com/photos/random?client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`
+)
+  .then((response) => response.json())
+  .then((url) => {
+    image.src = url.urls.full;
+    page.appendChild(image);
+  })
+  .catch(console.log('нет фотки!!!'));
