92f8ae9c251373406c27280ab4f57725c6e09401
Michael Mgebrishvili <mmd920802@gmail.com>
2023-11-02 21:16:00 +0300

    refactor code

--- 5.3 Work with API/The webpage is not the same without a cat/script.js
+++ 5.3 Work with API/The webpage is not the same without a cat/script.js
@@ -1,16 +1,16 @@
-import {API_KEY} from './apiKey.js';
+import { API_KEY } from './apiKey.js';
 
-const catGallery = document.querySelector(".cat-gallery");
-const selectCats = document.querySelector(".cat-gallery__select");
-const catPhotos = document.querySelector(".cat-photos");
+const catGallery = document.querySelector('.cat-gallery');
+const selectCats = document.querySelector('.cat-gallery__select');
+const catPhotos = document.querySelector('.cat-photos');
 
 selectCats.disabled = true;
 
-fetch("https://api.thecatapi.com/v1/breeds")
+fetch('https://api.thecatapi.com/v1/breeds')
   .then((response) => response.json())
   .then((dataset) => {
     for (const name of dataset) {
-      const option = document.createElement("option");
+      const option = document.createElement('option');
       option.textContent = name.name;
       option.value = name.id;
       selectCats.appendChild(option);
@@ -20,20 +20,21 @@ fetch("https://api.thecatapi.com/v1/breeds")
     selectCats.disabled = false;
   });
 
-selectCats.addEventListener("input", (event) => {
-  const loader = document.createElement("div");
-  loader.classList.add("loader");
+  const loader = document.createElement('div');
+
+selectCats.addEventListener('input', (event) => {
+  loader.classList.add('loader');
   catGallery.appendChild(loader);
   selectCats.disabled = true;
   const choice = event.target.value;
   fetch(`https://api.thecatapi.com/v1/images/search?limit=15&breed_ids=${choice}&api_key=${API_KEY}`)
     .then((response) => response.json())
     .then((photoList) => {
-      catPhotos.innerHTML = "";
+      catPhotos.innerHTML = '';
 
       for (let catPhoto of photoList) {
-        const image = document.createElement("img");
-        image.classList.add("photo");
+        const image = document.createElement('img');
+        image.classList.add('photo');
         image.src = catPhoto.url;
 
         catPhotos.appendChild(image);
@@ -41,6 +42,6 @@ selectCats.addEventListener("input", (event) => {
     })
     .finally(() => {
       selectCats.disabled = false;
-      loader.remove();
+      loader.classList.remove('loader');
     });
 });
