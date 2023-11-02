1bdf4574d06395257019adbdd0e0483ccdc9db08
Michael Mgebrishvili <mmd920802@gmail.com>
2023-11-02 22:23:55 +0300

    refactor promise

--- 5.3 Work with API/Theme of the day/script.js
+++ 5.3 Work with API/Theme of the day/script.js
@@ -11,17 +11,17 @@ fetch(
     return fetch(
       `https://api.unsplash.com/search/photos?query=${word.word}&client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`
     )
-      .then((response) => response.json())
-      .then((url) => {
-        image.src = url.results[0].urls.full;
-        page.appendChild(image);
-      })
-      .catch(() => {'error!'});
+  })
+  .then((response) => response.json())
+  .then((url) => {
+    image.src = url.results[0].urls.full;
+    page.appendChild(image);
   })
   .finally(() => {
     const loader = document.querySelector('.lds-ring');
     loader.classList.add('hidden');
-  });
+  })
+  .catch(() => { 'error!' });
 
 fetch(
   `https://api.unsplash.com/photos/random?client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`
