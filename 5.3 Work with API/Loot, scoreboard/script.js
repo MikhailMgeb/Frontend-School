4fa93e0086bf5889945468dcbd8aae73e7feb57f
Michael Mgebrishvili <mmd920802@gmail.com>
2023-11-02 21:52:28 +0300

    fix setInterval

--- 5.3 Work with API/Loot, scoreboard/script.js
+++ 5.3 Work with API/Loot, scoreboard/script.js
@@ -1,18 +1,22 @@
-import {API_KEY} from './apiKey.js';
+import { API_KEY } from './apiKey.js';
 const valueRUB = document.querySelector('.scoreboard__rub');
 const valueGEL = document.querySelector('.scoreboard__gel');
 
 //const exchangeRates = fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY}`)
 
-const exchangeRates = fetch(`http://127.0.0.1:5500/date.json`)
-  .then((response) => response.json())
-  .then((data) => {
-    clearInterval(exchangeRates);
-    valueRUB.textContent = data.rates.RUB.toFixed(2) + ' RUB';
-    valueGEL.textContent = data.rates.GEL.toFixed(2) + ' GEL';
-  })
-  .finally(() => {
-    setInterval(exchangeRates, 50000);
-  });
+function exchangeRates() {
+  fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+      valueRUB.textContent = Number(data.rates.RUB).toFixed(2) + ' RUB';
+      valueGEL.textContent = Number(data.rates.GEL).toFixed(2) + ' GEL';
 
+      const timerId = setTimeout(() => {
+        exchangeRates()
+      }, 10000)
+    })
+    .finally(() => {
+    });
+}
 
+exchangeRates()
