dca0942a0a11a2b077439834a06dd33451bb1b32
Michael Mgebrishvili <mmd920802@gmail.com>
2023-11-06 16:15:11 +0300

    refactor

--- 5.3 Work with API/Theme of the day/script.js
+++ 5.3 Work with API/Theme of the day/script.js
@@ -14,6 +14,19 @@ fetch(
   })
   .then((response) => response.json())
   .then((url) => {
+
+    if (url.total === 0) {
+      fetch(
+        `https://api.unsplash.com/photos/random?client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`
+      )
+        .then((response) => response.json())
+        .then((url) => {
+          image.src = url.urls.full;
+          page.appendChild(image);
+        })
+        .catch(console.log('нет фотки!!!'));
+    }
+
     image.src = url.results[0].urls.full;
     page.appendChild(image);
   })
@@ -22,13 +35,3 @@ fetch(
     loader.classList.add('hidden');
   })
   .catch(() => { 'error!' });
-
-fetch(
-  `https://api.unsplash.com/photos/random?client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`
-)
-  .then((response) => response.json())
-  .then((url) => {
-    image.src = url.urls.full;
-    page.appendChild(image);
-  })
-  .catch(console.log('нет фотки!!!'));
