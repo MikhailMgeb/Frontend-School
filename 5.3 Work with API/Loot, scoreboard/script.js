06bff3b67436a6e53901640007ff5d833b517cde
Michael Mgebrishvili <mmd920802@gmail.com>
2023-11-06 16:22:51 +0300

    refactor timer

--- 5.3 Work with API/Loot, scoreboard/script.js
+++ 5.3 Work with API/Loot, scoreboard/script.js
@@ -2,20 +2,17 @@ import { API_KEY } from './apiKey.js';
 const valueRUB = document.querySelector('.scoreboard__rub');
 const valueGEL = document.querySelector('.scoreboard__gel');
 
-//const exchangeRates = fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY}`)
-
 function exchangeRates() {
   fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY}`)
     .then((response) => response.json())
     .then((data) => {
       valueRUB.textContent = Number(data.rates.RUB).toFixed(2) + ' RUB';
       valueGEL.textContent = Number(data.rates.GEL).toFixed(2) + ' GEL';
-
+    })
+    .finally(() => {
       const timerId = setTimeout(() => {
         exchangeRates()
       }, 10000)
-    })
-    .finally(() => {
     });
 }
 
